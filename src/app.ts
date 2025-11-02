a4c429fb17b338ab452da76d85858a8b409e44ab
Lucca <luccamoretii@gmail.com>
2025-11-01 21:22:19 -0300

    se agrego la sanitizedinput function

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { Cancha } from './cancha/cancha.entity.js';
 import { Turno } from './turno/turno.entity.js';
 import { Material } from './material/material.entity.js';
+import { Tamanio } from './tamanio/tamanio.entity.js';
+import { Usuario } from './usuario/usuario.entity.js';
+import { Reserva } from './reserva/reserva.entity.js';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const canchas = [
-    new Cancha('Hola', 'en_punto'),
-    new Cancha('que onda', 'y_media'),
+    new Cancha('eren', 'en_punto'),
+    new Cancha('mikasa', 'y_media'),
 ];
 
 const turnos = [
@@ -23,6 +27,30 @@ const materiales = [
     new Material('Futbol_11', 'sin_techo','cesped')
 ];
 
+const tamanios = [
+    new Tamanio('chica',5,20,40),
+    new Tamanio('grande', 11, 60,100)
+]
+
+const usuarios = [
+    new Usuario('Lucca', 'Moretti','luccam@gmail.com','luccamoretti123','ADMIN'),
+    new Usuario('Leon', 'Juan Bennazar','leonjb@gmail.com','leonjb123','ADMIN')
+]
+
+const reservas = [
+    new Reserva('pendiente',new Date('2025-11-02T16:00:00'),usuarios[0].id,turnos[0].id, canchas[0].id),
+    new Reserva('confirmada',new Date('2025-11-01T17:00:00'),usuarios[1].id,turnos[1].id,canchas[1].id)
+]
+
+function sanitizedCanchaInput(req: Request, res: Response, next: NextFunction){
+    req.body.sanitizedInput = {
+        nombre: req.body.nombre,
+        tipo_turno: req.body.tipo_turno,
+    }
+    // aca se deberian hacer mas validaciones que luego vamos a ver, como el tipo de dato, que no haya contenido malisioso, operaciones extrañas, etc
+    next()
+}
+
 app.get('/api/canchas', (req, res) => {
   res.json(canchas);
 });
@@ -32,12 +60,33 @@ app.get('/api/turnos', (req, res) => {
 app.get('/api/materiales', (req, res) => {
   res.json(materiales);
 });
-
-app.post('/api/canchas', (req, res) => {
-  const { nombre, tipo_turno } = req.body;
-  const cancha = new Cancha(nombre, tipo_turno);
-  canchas.push(cancha);
+app.get('/api/tamanios', (req, res) => {
+  res.json(tamanios);
 });
+app.get('/api/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+app.get('/api/reservas', (req, res) => {
+  res.json(reservas);
+});
+
+app.post('/api/canchas', sanitizedCanchaInput,(req, res) => {
+  const input = req.body.sanitizedInput;
+  const cancha = new Cancha(input.nombre, input.tipo_turno);
+  canchas.push(cancha);
+  res.status(201).send({message: 'Cancha creada', data:cancha}) //el status indica que el recurso se creo correctamente, mas adelante se detallan los dif tipos de status
+});
+
+app.put('/api/canchas/:id',sanitizedCanchaInput, (req, res) => {
+    const canchaIdx = canchas.findIndex(c => c.id === Number(req.params.id)) //cambia el tipo de dato del id xq es number y req.params.id es string, otra forma es cambiar el tipo de dato del id
+    if(canchaIdx===-1){
+        res.status(404).send({menssage: 'Cancha no encontrada'})
+    }
+
+    canchas[canchaIdx] = {...canchas[canchaIdx], ...req.body.sanitizedInput} //generamos la modificacion de los datos sobre el objeto
+
+    res.status(200).send({menssage: 'La cancha se actualizo correctamente', data:canchas[canchaIdx]}) 
+})
 
 app.use('/', (req, res) => {
   res.send('hola');
