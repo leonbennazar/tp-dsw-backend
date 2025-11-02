068c331a30a5e566ce6e3a090c628181424177d3
Lucca <luccamoretii@gmail.com>
2025-11-02 20:33:42 -0300

    agrego patch y delete y metodos de unhandled errors

--- src/app.ts
+++ src/app.ts
@@ -47,6 +47,11 @@ function sanitizedCanchaInput(req: Request, res: Response, next: NextFunction){
         nombre: req.body.nombre,
         tipo_turno: req.body.tipo_turno,
     }
+    Object.keys(req.body.sanitizedInput).forEach((key)=> {
+      if(req.body.sanitizedInput[key] === undefined){
+        delete req.body.sanitizedInput[key]
+      }
+    })
     // aca se deberian hacer mas validaciones que luego vamos a ver, como el tipo de dato, que no haya contenido malisioso, operaciones extrañas, etc
     next()
 }
@@ -74,23 +79,35 @@ app.post('/api/canchas', sanitizedCanchaInput,(req, res) => {
   const input = req.body.sanitizedInput;
   const cancha = new Cancha(input.nombre, input.tipo_turno);
   canchas.push(cancha);
-  res.status(201).send({message: 'Cancha creada', data:cancha}) //el status indica que el recurso se creo correctamente, mas adelante se detallan los dif tipos de status
+  return res.status(201).send({message: 'Cancha creada', data:cancha}) //el status indica que el recurso se creo correctamente, mas adelante se detallan los dif tipos de status
 });
 
-app.put('/api/canchas/:id',sanitizedCanchaInput, (req, res) => {
+//se cambio el verbo de put a patch, es la misma funcion
+app.patch('/api/canchas/:id',sanitizedCanchaInput, (req, res) => {
     const canchaIdx = canchas.findIndex(c => c.id === Number(req.params.id)) //cambia el tipo de dato del id xq es number y req.params.id es string, otra forma es cambiar el tipo de dato del id
     if(canchaIdx===-1){
-        res.status(404).send({menssage: 'Cancha no encontrada'})
+        return res.status(404).send({menssage: 'Cancha no encontrada'})
     }
 
     canchas[canchaIdx] = {...canchas[canchaIdx], ...req.body.sanitizedInput} //generamos la modificacion de los datos sobre el objeto
 
-    res.status(200).send({menssage: 'La cancha se actualizo correctamente', data:canchas[canchaIdx]}) 
+    return res.status(200).send({menssage: 'La cancha se actualizo correctamente', data:canchas[canchaIdx]}) 
 })
 
-app.use('/', (req, res) => {
-  res.send('hola');
-});
+app.delete('/api/canchas/:id', (req,res)=> {
+  const canchaIdx = canchas.findIndex(c => c.id === Number(req.params.id))
+
+  if (canchaIdx === -1){
+    res.status(404).send({message: 'Cancha no encontrada'})
+  }else{
+    canchas.splice(canchaIdx, 1)
+    res.status(200).send({message: 'Cancha eliminada correctamente'})
+  }
+})
+
+app.use((_, res) => {
+  return res.status(404).send({ message: 'Direccion no encontrada' })
+})
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000/');
