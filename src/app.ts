67f5a45e7b87e8d07f761603728ba935ba3a16f7
Leon <143452196+leonbennazar@users.noreply.github.com>
2025-11-03 23:54:54 -0300

    cors config
    por seguridad, que solo reciba metodos de mi app de svelte

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ import { Usuario } from './usuario/usuario.entity.js';
 import { Reserva } from './reserva/reserva.entity.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 const canchas = [
