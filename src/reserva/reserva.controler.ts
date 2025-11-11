09d797bf1059e8382eb2f6f147bdfcb7c07224ff
Leon <143452196+leonbennazar@users.noreply.github.com>
2025-11-11 01:01:06 -0300

    minor fixes

--- src/reserva/reserva.controler.ts
+++ src/reserva/reserva.controler.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { Reserva } from './reserva.entity.js';
-import { orm } from '../shared/db/orm.js';  
+import { orm } from '../shared/db/orm.js';
 
 const em = orm.em;
 
@@ -12,9 +12,8 @@ function sanitizedReservaInput(
   req.body.sanitizedInput = {
     estado_reserva: req.body.estado_reserva,
     fecha_reserva: req.body.fecha_reserva,
-    usuario: req.body.usuario,
-    turno: req.body.turno,
-    cancha: req.body.cancha,
+    turno: Number(req.body.turno),
+    cancha: Number(req.body.cancha),
   };
 
   Object.keys(req.body.sanitizedInput).forEach((key) => {
@@ -27,55 +26,61 @@ function sanitizedReservaInput(
 }
 
 async function findAll(req: Request, res: Response) {
-  try{
-    const reservas = await em.find(Reserva, {}, {populate: ['turno', 'cancha']});
+  try {
+    const reservas = await em.find(
+      Reserva,
+      {},
+      { populate: ['turno', 'cancha'] }
+    );
     res.status(200).json({ message: 'Reservas encontradas', data: reservas });
-  } catch (error: any){
-    res.status(500).json({ message: error.message });
-}
-}
-
-
-async function findOne(req: Request, res: Response) {
-  try{
-    const id = Number.parseInt(req.params.id);
-    const reserva = await em.findOneOrFail(Reserva, {id}, {populate: ['turno', 'cancha']}); // el 2do parametro es un filtro
-    res.status(200).json({ message: 'Reserva encontrada', data: reserva });
-  } catch (error: any){
-    res.status(500).json({ message: error.message });
-}
-}
-
-async function add(req: Request, res: Response) {
-  try{
-    const reserva = em.create(Reserva, req.body.sanitizedInput);
-    await em.flush()
-    res.status(201).json({ message: 'Reserva creada', data: reserva });
-} catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
 
+async function findOne(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const reserva = await em.findOneOrFail(
+      Reserva,
+      { id },
+      { populate: ['turno', 'cancha'] }
+    ); // el 2do parametro es un filtro
+    res.status(200).json({ message: 'Reserva encontrada', data: reserva });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function add(req: Request, res: Response) {
+  try {
+    const reserva = em.create(Reserva, req.body.sanitizedInput);
+    await em.flush();
+    res.status(201).json({ message: 'Reserva creada', data: reserva });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 async function update(req: Request, res: Response) {
-  try{
+  try {
     const id = Number.parseInt(req.params.id);
     const reserva = em.getReference(Reserva, id);
     em.assign(reserva, req.body);
     await em.flush();
     res.status(200).json({ message: 'Reserva actualizado', data: reserva });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
 
 async function remove(req: Request, res: Response) {
-  try{
+  try {
     const id = Number.parseInt(req.params.id);
     const reserva = em.getReference(Reserva, id);
     await em.removeAndFlush(reserva);
     res.status(200).json({ message: 'Reserva eliminado' });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
--- src/turno/turno.controler.ts
+++ src/turno/turno.controler.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { Turno } from './turno.entity.js';
-import { orm } from '../shared/db/orm.js';  
+import { orm } from '../shared/db/orm.js';
 
 const em = orm.em;
 
@@ -21,55 +21,58 @@ function sanitizedTurnoInput(req: Request, res: Response, next: NextFunction) {
 
 async function findAll(req: Request, res: Response) {
   try {
-    const turnos = await em.find(Turno, {}); 
-    res.status(200).json({ message: 'Turnos encontrados', data: turnos });  
+    const turnos = await em.find(Turno, {}, { populate: ['reservas'] });
+    res.status(200).json({ message: 'Turnos encontrados', data: turnos });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
-}
+  }
 }
 
 async function findOne(req: Request, res: Response) {
-  try{
+  try {
     const id = Number.parseInt(req.params.id);
-    const turno =  await em.findOneOrFail(Turno, {id});
+    const turno = await em.findOneOrFail(
+      Turno,
+      { id },
+      { populate: ['reservas'] }
+    );
     res.status(200).json({ message: 'Turno encontrado', data: turno });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
-}
+  }
 }
 
 async function add(req: Request, res: Response) {
-  try{
+  try {
     const turno = em.create(Turno, req.body.sanitizedInput);
-    await em.flush()
+    await em.flush();
     res.status(201).json({ message: 'Turno creado', data: turno });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
 
 async function update(req: Request, res: Response) {
-  try{
+  try {
     const id = Number.parseInt(req.params.id);
     const turno = em.getReference(Turno, id);
     em.assign(turno, req.body);
     await em.flush();
     res.status(200).json({ message: 'Turno actualizado', data: turno });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
 
 async function remove(req: Request, res: Response) {
-  try{
+  try {
     const id = Number.parseInt(req.params.id);
     const turno = em.getReference(Turno, id);
     await em.removeAndFlush(turno);
     res.status(200).json({ message: 'Turno eliminado' });
-  }catch (error: any){
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
 
-
-export { sanitizedTurnoInput, findAll, findOne, add, update, remove }
+export { sanitizedTurnoInput, findAll, findOne, add, update, remove };
