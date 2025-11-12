28b810b4cf8087b15f6db2751bd2e0f1f6135554
Lucca <luccamoretii@gmail.com>
2025-11-12 18:31:24 -0300

    Minor fixes

--- src/reserva/reserva.entity.ts
+++ src/reserva/reserva.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Property, ManyToOne, Rel } from '@mikro-orm/core';
+import { Entity, Property, ManyToOne, Rel, Cascade } from '@mikro-orm/core';
 import { Turno } from '../turno/turno.entity.js';
 import { BaseEntity } from '../shared/db/baseEntity.entity.js';
 import { Cancha } from '../cancha/cancha.entity.js';
@@ -11,6 +11,6 @@ export class Reserva extends BaseEntity {
   fecha_reserva!: Date;
   @ManyToOne(() => Turno, { nullable: false })
   turno!: Rel<Turno>;
-  @ManyToOne(() => Cancha, { nullable: false })
+  @ManyToOne(() => Cancha, { nullable: false, cascade: [Cascade.ALL] },)
   cancha!: Rel<Cancha>;
 }
